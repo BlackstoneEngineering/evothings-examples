5bf8707f7e871ccb0af89a0f2905eb50be3488de
Austin Blackstone <austinblackstoneengineering@gmail.com>
2015-01-29 12:22:27 -0600

    polished ibeacon scan app. Added check for ibeacon prefix

--- experiments/mbed-iBeacon-scan/app.js
+++ experiments/mbed-iBeacon-scan/app.js
@@ -1,4 +1,4 @@
-// JavaScript code for the mbedTest app
+// JavaScript code for the mbed ibeacon scan app
 
 // Short name for EasyBLE library.
 var easyble = evothings.easyble;
@@ -6,7 +6,7 @@ var easyble = evothings.easyble;
 // Object that holds application data and functions.
 var app = {};
 
-// Object that holds beacon devices (if they have advertisementData.kCBAdvDataManufacturerData )
+// Object that holds found beacon devices
 var beacons = {};
 
 // Timer that displays list of beacons.
@@ -26,8 +26,8 @@ app.onDeviceReady = function()
 {
 	console.log('Device Ready!');
 
-	// only list devices once
-	easyble.reportDeviceOnce(true);
+	// only list devices once, useful if there are alot of beacons in the room
+	//easyble.reportDeviceOnce(true);
 
 	// Start Scanning
 	app.startScan()
@@ -49,22 +49,14 @@ app.startScan = function()
 	easyble.startScan(
 		function(device)
 		{
-			// print out debug information
-			//for(key in device){console.log("\tdevice."+key+":" +device[key])}
-			//console.log("\n\r\tname:"+device.name+"  address:"+device.address +" rssi:"+device.rssi +"\n\t ScanRecord:"+device.scanRecord)
-			//for(key in device.advertisementData){
-			//	console.log("\t"+device.name+".advertisementData."+key+"="+device.advertisementData[key])}
-			
-			// test BLE devices for advertisementData.kCBAdvDataManufacturerData
-			// beacon data is stored in the manufacturer data field
-			//if(device.advertisementData.kCBAdvDataManufacturerData != undefined ){
-			//	console.log(evothings.util.toHexRawData(device.advertisementData.kCBAdvDataManufacturerData))
-			//}
-
-			// Check for Manufacturer Data, necessary for beacons
-			// may need to add additional checks here to verify beacons
-			if (device.advertisementData.kCBAdvDataManufacturerData != undefined)
+			// Check for beacon Prefix = ibeacon prefix
+			// YOU CAN MODIFY THIS TO FIND THINGS OTHER THAN iBEACONS!!
+			if (evothings.util.toHexRawData(device.scanRecord).slice(0,18) == '0201061aff4c000215' )
 			{
+				if(beacons[device.address] == undefined){
+					app.showInfo("Beacons Found!")
+					console.log(evothings.util.toHexRawData(device.scanRecord))
+				}
 				// add device to list of possible beacons
 				beacons[device.address]=device;
 			}
@@ -76,9 +68,9 @@ app.startScan = function()
 		});
 };
 
+// Dynamically update the list of beacons displayed on the screen
 app.displayBeaconList = function()
 {
-	//console.log("HEY YOU! Hurry up and write the DisplayBeaconList function!")
 	// Clear beacon list.
 	$('#found-beacons').empty();
 
@@ -87,22 +79,30 @@ app.displayBeaconList = function()
 		// get device out of list
 		device = beacons[thing]
 		// get raw advertising data from ManufacturerData field
-		RawData = evothings.util.toHexRawData(device.advertisementData.kCBAdvDataManufacturerData)
-		//console.log(device.address +":"+RawData)
+		RawData = evothings.util.toHexRawData(device.scanRecord).slice(0,62)
+		// extract data fields from Raw Data 
+		// remember, each byte takes up 2 cahracters, so the numbers are *2
+		prefix 	=RawData.slice(0,18)	// 9byte*2 = 18
+		proxUUID=RawData.slice(18,50)	// 16byte*2 = 32
+		minor  	=RawData.slice(50,54)	
+		major 	=RawData.slice(54,58)
+		txPower =RawData.slice(58,60)
 		
 		var element = $(
 			'<li>'
-			+		'UUID: ' +device.uuid 	+'<br />'
-			+		'Address: ' +device.address 	+'<br />'
-			+		'RSSI: ' +device.rssi 	+'<br />'
-			+		'ManufacturerData: ' +RawData 		+'<br />'
+			+		'Address: '+device.address  +'<br />'
+			+		'RSSI: '+device.rssi 		+'<br />'
+			+		'Raw Data: 0x' +RawData 	+'<br />'
+			+		'Prefix:   0x' +prefix 		+'<br />'
+			+		'ProxUUID: 0x'+proxUUID 	+'<br />'
+			+		'Major:   0x'+major  +' ('+parseInt(major,16)  +')' +'<br />'
+			+		'Minor:   0x'+minor  +' ('+parseInt(minor,16)  +')'	+'<br />'
+			+		'TxPower: 0x'+txPower+' ('+parseInt(txPower,16)+')' +'<br />'
 			+'	<li>'
 			);
 		$('#found-beacons').append(element);
 	}
-
 }
-
 
 // Initialize the app.
 app.initialize();
